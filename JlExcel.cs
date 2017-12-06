287a337125eeb098f48c844ed7e733b24066ac58
Jason <522454279@qq.com>
2017-12-06 18:10:21 +0800

    增加导出Excel相关方法

--- JlExcel.cs
+++ JlExcel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
 using System.Data;
 using System.Data.OleDb;
 using System.IO;
@@ -15,13 +18,13 @@ namespace JasonLib
         ///// <param name="fileName">文件名</param>
         ///// <param name="content">每一行用'\n\t'分隔，每一列用','分隔</param>
         ///// <returns>返回地址</returns>
-        public static string ExportCsv(string path, string fileName, string content)
+        public static string ExportCvs(string path, string fileName, string content, string format = "csv")
         {
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
             }
-            fileName += ".csv";
+            fileName += "." + format;
             var url = path + fileName;
             if (File.Exists(url))
             {
@@ -41,7 +44,145 @@ namespace JasonLib
         /// </summary>
         /// <param name="fileName">文件地址</param>
         /// <returns></returns>
+        public static IWorkbook ExportToWorkbook(DataSet ds, JlWorkbookType wbType = JlWorkbookType.HSSF)
+        {
+            IWorkbook wb;
+            switch (wbType)
+            {
+                case JlWorkbookType.XSSF: wb = new XSSFWorkbook(); break;
+                case JlWorkbookType.HSSF: wb = new HSSFWorkbook(); break;
+                default: wb = new HSSFWorkbook(); break;
+            }
+
+            var sheetsNum = wb.NumberOfSheets;
+            var sheet1 = wb.CreateSheet("sheet1");
+
+            var rowHeader = sheet1.CreateRow(0);
+
+            for (var i = 0; i < ds.Tables[0].Columns.Count; i++)
+            {
+                var cell = rowHeader.CreateCell(i);
+                cell.SetCellType(CellType.String);
+                cell.SetCellValue(ds.Tables[0].Columns[i].ToString());
+            }
+
+            for (var j = 0; j < ds.Tables[0].Rows.Count; j++)
+            {
+                var row = sheet1.CreateRow(j + 1);
+                for (var k = 0; k < ds.Tables[0].Columns.Count; k++)
+                {
+                    var cell = row.CreateCell(k);
+                    switch (ds.Tables[0].Columns[k].DataType.Name.ToLower())
+                    {
+                        case "string":
+                            cell.SetCellType(CellType.String);
+                            cell.SetCellValue(ds.Tables[0].Rows[j][k].ToString());
+                            break;
+                        case "bool":
+                            cell.SetCellType(CellType.Boolean);
+                            cell.SetCellValue(JlConvert.TryToBool(ds.Tables[0].Rows[j][k]));
+                            break;
+                        case "int32":
+                        case "decimal":
+                            cell.SetCellType(CellType.Numeric);
+                            cell.SetCellValue(JlConvert.TryToDouble(ds.Tables[0].Rows[j][k]));
+                            break;
+                        default:
+                            cell.SetCellType(CellType.String);
+                            cell.SetCellValue(ds.Tables[0].Rows[j][k].ToString());
+                            break;
+                    }
+                }
+            }
+
+            return wb;
+        }
+
+        /// <summary>
+        /// 将Excel内容存入DataSet中(需引用NPOI相关DLL)
+        /// </summary>
+        /// <param name="fileName">文件地址</param>
+        /// <returns></returns>
         public static DataSet ImportXlsToDataSet(string fileName)
+        {
+            var dataSet = new DataSet();
+
+            IWorkbook wb;
+
+            try
+            {
+                wb = new XSSFWorkbook(new FileStream(fileName, FileMode.Open));
+            }
+            catch
+            {
+                wb = new HSSFWorkbook(new FileStream(fileName, FileMode.Open));
+            }
+            var sheetsNum = wb.NumberOfSheets;
+
+            for (var i = 0; i < sheetsNum; i++)
+            {
+                var dataTable = new DataTable();
+                var sht = wb.GetSheetAt(i);
+                dataTable.TableName = sht.SheetName;
+                var enu = sht.GetRowEnumerator();
+
+                //获取最大列数
+                var maxCellNum = 0;
+                while (enu.MoveNext())
+                {
+                    var currentRow = (XSSFRow)enu.Current;
+                    if (maxCellNum < currentRow.LastCellNum)
+                    {
+                        maxCellNum = currentRow.LastCellNum;
+                    }
+                }
+                enu.Reset();
+                // 设置datatable字段
+                for (int j = 0, len = sht.GetRow(0).LastCellNum; j < maxCellNum; j++)
+                {
+                    if (j >= sht.GetRow(0).FirstCellNum && j < sht.GetRow(0).LastCellNum)
+                    {
+                        dataTable.Columns.Add(sht.GetRow(0).Cells[j].StringCellValue);
+                    }
+                    else
+                    {
+                        dataTable.Columns.Add(string.Empty);
+                    }
+                }
+                while (enu.MoveNext())
+                {
+                    var currentRow = (XSSFRow)enu.Current;
+                    var dataRow = dataTable.NewRow();
+                    for (var k = currentRow.FirstCellNum; k < currentRow.LastCellNum; k++)
+                    {
+                        var cell = currentRow.GetCell(k);
+
+                        switch (cell.CellType)
+                        {
+                            case CellType.Numeric: dataRow[k] = cell.NumericCellValue; break;
+                            case CellType.Boolean: dataRow[k] = cell.BooleanCellValue; break;
+                            case CellType.String: dataRow[k] = cell.StringCellValue; break;
+                            case CellType.Blank: dataRow[k] = string.Empty; break;
+                            default:
+                                dataRow[k] = null; break;
+                        }
+
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+                dataSet.Tables.Add(dataTable);
+            }
+
+            return dataSet;
+
+        }
+
+        /// <summary>
+        /// 将Excel内容存入DataSet中(OFFICE)
+        /// </summary>
+        /// <param name="fileName">文件地址</param>
+        /// <returns></returns>
+        public static DataSet ImportXls2DataSet(string fileName)
         {
             if (fileName == string.Empty)
             {
@@ -75,5 +216,18 @@ namespace JasonLib
 
             return ds;
         }
+    }
+
+    public enum JlWorkbookType
+    {
+        /// <summary>
+        /// office2007
+        /// </summary>
+        XSSF,
+
+        /// <summary>
+        /// office2003
+        /// </summary>
+        HSSF
     }
 }
